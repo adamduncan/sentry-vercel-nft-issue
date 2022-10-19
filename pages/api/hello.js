c02eb2c1a1b158473d4c4ad09d6e0f50817c6b32
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-19 23:04:37 +0100

    Move data

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from "node:fs";
+import path from "node:path";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res
+    .status(200)
+    .json(JSON.parse(fs.readFileSync(path.resolve("data.json"), "utf8")));
 }
